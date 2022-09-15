43c20f17d2ba4893da14ea957c1449d98520936d
Jhon <jhon@jhon.Ienovo>
2022-09-15 20:11:31 -0300

    configurando ambiente node da aplicacao

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+
+app.get('/ads', (request, response) => {
+    return response.json([
+        {id:1, name: 'Teste 1'},
+        {id:2, name: 'Teste 2'},
+        {id:3, name: 'Teste 3'}
+    ])
+})
+
+app.listen(3333)
